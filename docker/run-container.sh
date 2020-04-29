a1f2898d79c1ec3007ab2277d24c32dffad22403
jesse <jesse.a.lane@gmail.com>
2020-04-29 14:36:50 +0000

    Added comments to Docker container run script.

--- docker/run-container.sh
+++ docker/run-container.sh
@@ -1,3 +1,7 @@
 #/bin/bash
 
+# Run with GPU support  TODO: I should change the mapped directory to an env var
 docker run -d --runtime=nvidia --name=ace-net --volume /home/jesse/git/ace-net:/workspace --entrypoint= jalane76/ace-net:latest tail -f /dev/null
+
+# Uncomment to run without GPU support
+# docker run -d --name=ace-net --volume /home/jesse/git/ace-net:/workspace --entrypoint= jalane76/ace-net:latest tail -f /dev/null

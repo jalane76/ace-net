1a0e3dbb19f240fe52af2b1091a64ff44b0f52cf
Jesse <jesse.a.lane@gmail.com>
2020-04-28 21:59:53 -0500

    Updated run script to use image pulled from DockerHub registry

--- docker/run-container.sh
+++ docker/run-container.sh
@@ -1,3 +1,3 @@
 #/bin/bash
 
-docker run -d --runtime=nvidia --name=ace-net --volume /home/jesse/git/ace-net:/workspace --entrypoint= ace-net:latest tail -f /dev/null
+docker run -d --runtime=nvidia --name=ace-net --volume /home/jesse/git/ace-net:/workspace --entrypoint= jalane76/ace-net:latest tail -f /dev/null

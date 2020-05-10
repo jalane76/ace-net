51e620037d8f11d629572db302e22a0a34d7fbaf
jesse <jesse.a.lane@gmail.com>
2020-05-10 19:25:37 +0000

    Improved MNIST visualization and upgraded DVC.

--- src/visualization/viz_mnist.py
+++ src/visualization/viz_mnist.py
@@ -1,10 +1,12 @@
 # -*- coding: utf-8 -*-
 import click
 import matplotlib
+from matplotlib.colors import TwoSlopeNorm
 import matplotlib.pyplot as plt
 import numpy as np
 import os
 import torch
+from tqdm import tqdm, trange
 
 
 @click.command()
@@ -16,7 +18,6 @@ import torch
 @click.argument('output_path', type=click.Path())
 def main(ace_filepath, ie_filepath, interventions_filepath, x_train_filepath, y_train_filepath, output_path):
 
-    print('Loading...')
     H = W = 28
     ace = torch.load(ace_filepath).cpu()
     y_size = ace.size(1)
@@ -24,11 +25,13 @@ def main(ace_filepath, ie_filepath, interventions_filepath, x_train_filepath, y_
     ace = ace.reshape(H, W, y_size, i_size)
     # swap axes so they look right
     ace = np.swapaxes(ace.numpy(), 0, 1).astype(np.float32)
+    ace_val_limit = max(abs(ace.min()), ace.max())
 
     ie = torch.load(ie_filepath).cpu()
     ie = ie.reshape(H, W, y_size, i_size)
     # swap axes so they look right
     ie = np.swapaxes(ie.numpy(), 0, 1).astype(np.float32)
+    ie_val_limit = max(abs(ie.min()), ie.max())
 
     interventions = torch.load(interventions_filepath).cpu()
     num_alphas = interventions.nelement()
@@ -36,12 +39,10 @@ def main(ace_filepath, ie_filepath, interventions_filepath, x_train_filepath, y_
     y_train = torch.load(y_train_filepath)
     
     # collapse the one hot encodings to classes
-    print('Collapsing one hot encodings...')
     classes = [np.where(r == 1)[0][0] for r in y_train]
     num_classes = 10
     class_examples = np.zeros((H, W, num_classes))
 
-    print('Summing all classes')
     for idx, c in enumerate(classes):
         class_examples[:, :, c] += x_train[idx, 0, :, :]
 
@@ -54,46 +55,40 @@ def main(ace_filepath, ie_filepath, interventions_filepath, x_train_filepath, y_
         os.makedirs(output_path)
 
     # Plot the ACEs
-    print('Plotting...')
-    print('There are {} classes and {} interventions.'.format(num_classes, num_alphas))   
-    fig, axes = plt.subplots(num_alphas + 1, num_classes, sharex=True, sharey=True)
-    fig.set_figheight(4.0 * (num_alphas + 1))
-    fig.set_figwidth(4.0 * num_classes)
-    for class_index in range(num_classes):
-        if class_index >= y_size:
-            break
-        print('class: {}'.format(class_index))
-        class_axis = axes[0, class_index]
-        class_axis.imshow(class_examples[:, :, class_index], aspect='equal', interpolation='nearest')
-        
-        for alpha_index in range(num_alphas):
-            print('intervention: {}'.format(alpha_index))
-            alpha_axis = axes[alpha_index + 1, class_index]
-            alpha_axis.imshow(ace[:, :, class_index, alpha_index], aspect='equal', interpolation='nearest')
-    plot_name = 'mnist_ace.png'
-    fig.savefig(os.path.join(output_path, plot_name))
-    print('Saved {}'.format(plot_name))
+    total_iters = num_classes * num_alphas * 2
+    with tqdm(total_iters) as pbar:
+        fig, axes = plt.subplots(num_alphas + 1, num_classes, sharex=True, sharey=True)
+        fig.set_figheight(4.0 * (num_alphas + 1))
+        fig.set_figwidth(4.0 * num_classes)
+        for class_index in range(num_classes):
+            if class_index >= y_size:
+                break
+            class_axis = axes[0, class_index]
+            class_axis.imshow(class_examples[:, :, class_index], aspect='equal', interpolation='nearest')
+            
+            for alpha_index in range(num_alphas):
+                alpha_axis = axes[alpha_index + 1, class_index]
+                alpha_axis.imshow(ace[:, :, class_index, alpha_index], norm=TwoSlopeNorm(vmin=-ace_val_limit , vcenter=0, vmax=ace_val_limit), cmap='bwr', aspect='equal', interpolation='nearest')
+                pbar.update(1)
+        plot_name = 'mnist_ace.png'
+        fig.savefig(os.path.join(output_path, plot_name))
 
-    # Plot the IEs
-    print('Plotting...')
-    print('There are {} classes and {} interventions.'.format(num_classes, num_alphas))   
-    fig, axes = plt.subplots(num_alphas + 1, num_classes, sharex=True, sharey=True)
-    fig.set_figheight(4.0 * (num_alphas + 1))
-    fig.set_figwidth(4.0 * num_classes)
-    for class_index in range(num_classes):
-        if class_index >= y_size:
-            break
-        print('class: {}'.format(class_index))
-        class_axis = axes[0, class_index]
-        class_axis.imshow(class_examples[:, :, class_index], aspect='equal', interpolation='nearest')
-        
-        for alpha_index in range(num_alphas):
-            print('intervention: {}'.format(alpha_index))
-            alpha_axis = axes[alpha_index + 1, class_index]
-            alpha_axis.imshow(ie[:, :, class_index, alpha_index], aspect='equal', interpolation='nearest')
-    plot_name = 'mnist_ie.png'
-    fig.savefig(os.path.join(output_path, plot_name))
-    print('Saved {}'.format(plot_name))
+        # Plot the IEs 
+        fig, axes = plt.subplots(num_alphas + 1, num_classes, sharex=True, sharey=True)
+        fig.set_figheight(4.0 * (num_alphas + 1))
+        fig.set_figwidth(4.0 * num_classes)
+        for class_index in range(num_classes):
+            if class_index >= y_size:
+                break
+            class_axis = axes[0, class_index]
+            class_axis.imshow(class_examples[:, :, class_index], aspect='equal', interpolation='nearest')
+            
+            for alpha_index in range(num_alphas):
+                alpha_axis = axes[alpha_index + 1, class_index]
+                alpha_axis.imshow(ie[:, :, class_index, alpha_index], norm=TwoSlopeNorm(vmin=-ace_val_limit , vcenter=0, vmax=ace_val_limit), cmap='bwr', aspect='equal', interpolation='nearest')
+                pbar.update(1)
+        plot_name = 'mnist_ie.png'
+        fig.savefig(os.path.join(output_path, plot_name))
 
 
 

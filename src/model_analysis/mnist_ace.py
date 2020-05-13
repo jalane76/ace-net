3d849f44096e44557642e800e43265929bc79c65
jesse <jesse.a.lane@gmail.com>
2020-05-13 23:58:57 +0000

    Completed DVC stage refactor and repro'ed all stages.

--- src/model_analysis/mnist_ace.py
+++ src/model_analysis/mnist_ace.py
@@ -14,8 +14,10 @@ import torch.nn.functional as F
 @click.argument('clip_values_filepath', type=click.Path(exists=True))
 @click.argument('covariance_filepath', type=click.Path(exists=True))
 @click.argument('means_filepath', type=click.Path(exists=True))
-@click.argument('output_path', type=click.Path())
-def main(model_filepath, clip_values_filepath, covariance_filepath, means_filepath, output_path):
+@click.argument('interventional_expectations_output_path', type=click.Path())
+@click.argument('average_causal_effects_output_path', type=click.Path())
+@click.argument('interventions_output_path', type=click.Path())
+def main(model_filepath, clip_values_filepath, covariance_filepath, means_filepath, interventional_expectations_output_path, average_causal_effects_output_path, interventions_output_path):
     model = torch.load(model_filepath)
     cov = torch.load(covariance_filepath)
     mean = torch.load(means_filepath)
@@ -29,18 +31,15 @@ def main(model_filepath, clip_values_filepath, covariance_filepath, means_filepa
     interventions = torch.Tensor(np.linspace(*clip_values, num_alphas))
     ie = ace.interventional_expectation(model, mean, cov, interventions, epsilon=0.000001, method='hessian_diag', progress=True)
     avg_ce = ace.average_causal_effect(ie)
-
-    if not os.path.exists(output_path):
-        os.makedirs(output_path)
-
+    
     # Remove names for now since named tensors aren't serializable
     # Also detach grads so they are not saved.
     ie = ie.rename(None).detach()
     avg_ce = avg_ce.rename(None).detach()
 
-    torch.save(ie, os.path.join(output_path, 'interventional_expectations.pt'))
-    torch.save(avg_ce, os.path.join(output_path, 'average_causal_effects.pt'))
-    torch.save(torch.Tensor(interventions), os.path.join(output_path, 'interventions.pt'))
+    torch.save(ie, interventional_expectations_output_path)
+    torch.save(avg_ce, average_causal_effects_output_path)
+    torch.save(torch.Tensor(interventions), interventions_output_path)
 
 if __name__ == '__main__':
     main()

b44f6edd6841457f72bda5c93ce746c7edaabf8c
jesse <jesse.a.lane@gmail.com>
2020-05-28 00:17:52 +0000

    First round of ace predictions done.

--- src/model_analysis/predict_adv_class_using_ace.py
+++ src/model_analysis/predict_adv_class_using_ace.py
@@ -35,23 +35,49 @@ def main(ace_filepath, interventions_filepath, x_test_filepath, y_test_filepath,
     
     y_predicted_adv = np.zeros_like(y_test_adv)
 
+    # Interpolation function storage
+    interp_funcs = np.empty((x_test.shape[0], x_test.shape[1]), dtype=np.object)
+
     for n in trange(x_test.shape[0]):
+    #for n in trange(1):
         x = x_test[n, :]
         x_adv = x_test_adv[n, :]
         
-        diff = x - x_adv
+        #diff = x - x_adv
 
         effect_map = torch.zeros_like(ace[:, :, 0])
 
-        for idx in diff.nonzero()[0]:
+        #for idx in diff.nonzero()[0]:
+        for idx in range(x_adv.shape[0]):
             for c in range(num_classes):
-                f = interpolate.interp1d(interventions, ace[idx, c, :])
+                if not interp_funcs[idx, c]:
+                    interp_funcs[idx, c] = interpolate.interp1d(interventions, ace[idx, c, :])
+                f = interp_funcs[idx, c]
                 effect_map[idx, c] = torch.as_tensor(f(x_adv[idx]))
         effect_map = torch.sum(effect_map, dim=0)
         y_predicted_adv[n] = np.argmax(effect_map.numpy())
 
+    per_class_accuracy = {}
+    for c in range(num_classes):
+        per_class_accuracy[c] = {
+            'Correct': 0,
+            'Total': 0,
+            'ClassBin': [0] * num_classes
+        }
+    for y, pred_y in zip(y_test_adv, y_predicted_adv):
+        correct = 0
+        if y == pred_y:
+            correct = 1
+        per_class_accuracy[y]['Correct'] += correct
+        per_class_accuracy[y]['Total'] += 1
+        per_class_accuracy[y]['ClassBin'][pred_y] += 1
+    
+    for key, value in per_class_accuracy.items():
+        value['Accuracy'] = value['Correct'] / value['Total']
+
     metrics = {
-        'Accuracy': str(np.sum(y_test_adv == y_predicted_adv) / len(y_test_adv))
+        'Total Accuracy': str(np.sum(y_test_adv == y_predicted_adv) / len(y_test_adv)),
+        'Per Class Accuracy': per_class_accuracy
     }
 
     # Save data    

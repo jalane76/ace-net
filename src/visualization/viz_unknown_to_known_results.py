124c6a9adcfa6b3f20f9f643fdb24b1c12f20c1d
Jesse Lane <jesse.a.lane@gmail.com>
2020-07-21 14:51:01 +0000

    Cleaned up some of the visualizations.

--- src/visualization/viz_unknown_to_known_results.py
+++ src/visualization/viz_unknown_to_known_results.py
@@ -76,6 +76,40 @@ def plot_class_counts(df, output_path):
 
     fig.savefig(os.path.join(output_path, 'class_counts.png'))
 
+def plot_examples(df, img_size, data_filepath, output_path):
+
+    W, H = img_size
+    norms = df['Norm'].unique()
+    target_classes = df['Target Class'].unique()
+
+    rows = ['{}'.format(row) for row in target_classes]
+
+    for norm in norms:
+
+        fig, axes = plt.subplots(nrows=len(rows), ncols=1, figsize=(10, 10), sharex='col', sharey=True)
+        axes[0].set_title(norm, fontsize=15)
+        
+        for ax, row, target in zip(axes, rows, target_classes):
+            ax.set_ylabel(row, rotation=0, fontsize=20, verticalalignment='center')
+
+            plot_df = df[(df['Norm'] == norm) & (df['Target Class'] == target)]
+
+            plot = np.zeros((W, H * plot_df['Budget'].nunique()))
+            num_ticks = np.arange(plot_df['Budget'].nunique())
+            for idx, budget in enumerate(sorted(plot_df['Budget'].unique())):
+                path = os.path.join(data_filepath, 'attacks_target-{}_norm-{}_budget-{}.pt'.format(target, norm.strip('L_'), budget))
+                img = torch.load(path)[0, :]
+                img = img.reshape(W, H)
+                img = np.swapaxes(img.numpy(), 0, 1).astype(np.float32)
+                
+                plot[:, H * idx : (H * (idx + 1))] = img
+
+            ax.imshow(plot, aspect='equal', interpolation='nearest')
+            ax.set_xticks(num_ticks * W + W / 2)
+            ax.set_xticklabels('{:.2f}'.format(b) for b in sorted(plot_df['Budget'].unique()))
+
+        fig.savefig(os.path.join(output_path, 'examples_norm-{}.png'.format(norm)))
+
 @click.command()
 @click.argument('data_filepath', type=click.Path(exists=True))
 @click.argument('output_path', type=click.Path())
@@ -87,13 +121,14 @@ def main(data_filepath, output_path):
 
     H = W = 28
     num_classes = 10
-    df = pd.read_csv(os.path.join(data_filepath, 'unknown_to_known_permutations_results.csv'))
+    df = pd.read_csv(os.path.join(data_filepath, 'unknown_to_known_permutations_results.csv'), float_precision='round_trip')
 
     if not os.path.exists(output_path):
         os.makedirs(output_path)
 
     plot_accuracies(df, output_path)
     plot_class_counts(df, output_path)
+    plot_examples(df, (W, H), data_filepath, output_path)
 
     if not os.path.exists(output_path):
         os.makedirs(output_path)

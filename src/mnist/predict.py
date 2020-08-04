12dcce842ac7d5de929b9d4c80e80f613e005cd2
Jesse Lane <jesse.a.lane@gmail.com>
2020-08-04 18:38:31 +0000

    Updated benign prediction.

--- src/mnist/predict.py
+++ src/mnist/predict.py
@@ -2,56 +2,53 @@
 from art.classifiers import PyTorchClassifier
 import click
 import json
-from models.mnist_model import MnistModel
+import mnist.models
 import numpy as np
 import os
 import torch
 import torch.nn as nn
 import torch.nn.functional as F
+from common.utils import get_model_from_module
 
 @click.command()
-@click.argument('x_test_filepath', type=click.Path(exists=True))
-@click.argument('y_test_filepath', type=click.Path(exists=True))
+@click.argument('x_filepath', type=click.Path(exists=True))
+@click.argument('y_filepath', type=click.Path(exists=True))
 @click.argument('clip_values_filepath', type=click.Path(exists=True))
-@click.argument('model_filepath', type=click.Path(exists=True))
-@click.argument('optimizer_filepath', type=click.Path(exists=True))
+@click.argument('trained_model_filepath', type=click.Path(exists=True))
 @click.argument('metrics_output_path', type=click.Path())
-def main(x_test_filepath, y_test_filepath, clip_values_filepath, model_filepath, optimizer_filepath, metrics_output_path):
+def main(x_filepath, y_filepath, clip_values_filepath, trained_model_filepath, metrics_output_path):
 
     seed = 45616451
     np.random.seed(seed)
     torch.manual_seed(seed)
 
     # Load data
-    x_test = torch.load(x_test_filepath)
-    y_test = torch.load(y_test_filepath)
+    x = torch.load(x_filepath)
+    y = torch.load(y_filepath)
 
-    # Flatten x_test
-    x_test = x_test.reshape(x_test.shape[0], -1)
+    # Flatten
+    x = x.reshape(x.shape[0], -1)
 
-    model = torch.load(model_filepath)
-    optimizer = torch.load(optimizer_filepath)
+    model = torch.load(trained_model_filepath)
 
     clip_values = {}
     with open(clip_values_filepath, 'r') as f:
         clip_values = json.load(f)
     clip_values = (clip_values.get('min_pixel_value'), clip_values.get('max_pixel_value'))
-    
-    criterion = nn.CrossEntropyLoss()
 
     classifier = PyTorchClassifier(
         model=model,
         clip_values=clip_values,
-        loss=criterion,
-        optimizer=optimizer,
+        loss=model.criterion,
+        optimizer=model.optimizer,
         input_shape=(1, 28, 28),
         nb_classes=10
     )
 
     # Evaluate the classifier on benign data
-    predictions = classifier.predict(x_test)
+    predictions = classifier.predict(x)
     accuracy = {
-        'Accuracy': np.sum(np.argmax(predictions, axis=1) == np.argmax(y_test, axis=1)) / len(y_test)
+        'Accuracy': np.sum(np.argmax(predictions, axis=1) == np.argmax(y, axis=1)) / len(y)
     }
 
     # Save data    

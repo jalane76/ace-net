8a75c3719837cd537d6f0b039765f7af31433a0f
Jesse Lane <jesse.a.lane@gmail.com>
2020-08-05 19:57:08 +0000

    Reproduced ACE attack visualization.

--- src/mnist/viz_ace_attack.py
+++ src/mnist/viz_ace_attack.py
@@ -10,24 +10,24 @@ from tqdm import tqdm, trange
 
 
 @click.command()
-@click.argument('x_test_filepath', type=click.Path(exists=True))
-@click.argument('y_test_filepath', type=click.Path(exists=True))
-@click.argument('x_test_adv_filepath', type=click.Path(exists=True))
+@click.argument('x_filepath', type=click.Path(exists=True))
+@click.argument('y_filepath', type=click.Path(exists=True))
+@click.argument('x_adv_filepath', type=click.Path(exists=True))
 @click.argument('output_path', type=click.Path())
-def main(x_test_filepath, y_test_filepath, x_test_adv_filepath, output_path):
+def main(x_filepath, y_filepath, x_adv_filepath, output_path):
 
     seed = 45616451
     np.random.seed(seed)
     torch.manual_seed(seed)
 
     H = W = 28
-    x_test = torch.load(x_test_filepath)
-    y_test = torch.load(y_test_filepath)
-    x_test_adv = torch.load(x_test_adv_filepath).numpy()
+    x = torch.load(x_filepath)
+    y = torch.load(y_filepath)
+    x_adv = torch.load(x_adv_filepath).numpy()
 
     # swap axes so they look right
-    x_test = np.swapaxes(x_test, 2, 3).astype(np.float32)
-    x_test_adv = np.swapaxes(x_test_adv, 2, 3).astype(np.float32)
+    x = np.swapaxes(x, 2, 3).astype(np.float32)
+    x_adv = np.swapaxes(x_adv, 2, 3).astype(np.float32)
     
     num_classes = 10
 
@@ -39,10 +39,10 @@ def main(x_test_filepath, y_test_filepath, x_test_adv_filepath, output_path):
         fig.set_figwidth(4.0 * num_samples)
         for sample_idx in range(num_samples):
             sample_axis = axes[0, sample_idx]
-            sample_axis.imshow(x_test[sample_idx, 0, :, :], aspect='equal', interpolation='nearest')
+            sample_axis.imshow(x[sample_idx, 0, :, :], aspect='equal', interpolation='nearest')
 
             evil_twin_axis = axes[1, sample_idx]
-            evil_twin_axis.imshow(x_test_adv[sample_idx, 0, :, :], aspect='equal', interpolation='nearest')
+            evil_twin_axis.imshow(x_adv[sample_idx, 0, :, :], aspect='equal', interpolation='nearest')
         fig.savefig(output_path)
 
 if __name__ == '__main__':
